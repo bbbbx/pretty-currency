1b0485378dcd3c10bd4934367dfaa561e534424e
venus <venus@venusworld.cn>
2019-03-19 00:30:07 +0800

    update test case.

--- test/index.test.js
+++ test/index.test.js
@@ -1,13 +1,108 @@
 const prettyCurrency = require('pretty-currency')
 
-test('pretty 123456 to "123,456"', () => {
-    expect(prettyCurrency(123456)).toBe('123,456')
+describe('Integer', () => {
+    test('123456 should be "123,456"', () => {
+        expect(prettyCurrency(123456)).toBe('123,456')
+    })
+
+    test('12 should be "12"', () => {
+        expect(prettyCurrency(12)).toBe('12')
+    })
+
+    test('0 should be "123,456"', () => {
+        expect(prettyCurrency(0)).toBe('0')
+    })
+
+    test('-1 should be "-1"', () => {
+        expect(prettyCurrency(-1)).toBe('-1')
+    })
+
+    test('-1234 should be "-1,234"', () => {
+        expect(prettyCurrency(-1234)).toBe('-1,234')
+    })
+
+    test('-1234 should be "-1,234"', () => {
+        expect(prettyCurrency(-1234)).toBe('-1,234')
+    })
 })
 
-test('pretty 123456.1234 to "123,456.1234"', () => {
-    expect(prettyCurrency(123456.1234)).toBe('123,456.1234')
+describe('Float number', () => {
+    test('123456.123456 should be "123,456.123456"', () => {
+        expect(prettyCurrency(123456.123456)).toBe('123,456.123456')
+    })
+
+    test('123456.1234 should be "123,456.1234"', () => {
+        expect(prettyCurrency(123456.1234)).toBe('123,456.1234')
+    })
+
+    test('123456.1 should be "123,456.1"', () => {
+        expect(prettyCurrency(123456.1)).toBe('123,456.1')
+    })
+
+    test('123456. should be "123,456"', () => {
+        expect(prettyCurrency(123456.)).toBe('123,456')
+    })
+
+    test('123456.00 should be "123,456"', () => {
+        // JS engine will transform float number 123.00 to 123 integer
+        expect(prettyCurrency(123456.00)).toBe('123,456')
+    })
+
+    test('-12.12345 should be "-12.12345"', () => {
+        expect(prettyCurrency(-12.12345)).toBe('-12.12345')
+    })
+
+    test('-0.12345 should be "-0.12345"', () => {
+        expect(prettyCurrency(-0.12345)).toBe('-0.12345')
+    })
 })
 
-test('pretty "123456.1234" to "123,456.1234"', () => {
-    expect(prettyCurrency('123456.1234')).toBe('123,456.1234')
+describe('String', () => {
+    test('"123456.1234" should be "123,456.1234"', () => {
+        expect(prettyCurrency('123456.1234')).toBe('123,456.1234')
+    })
+
+    test('"123456." should be "123,456"', () => {
+        expect(prettyCurrency('123456.')).toBe('123,456')
+    })
+
+    test('"123456" should be "123,456"', () => {
+        expect(prettyCurrency('123456')).toBe('123,456')
+    })
+
+    test('"" should be ""', () => {
+        expect(prettyCurrency('')).toBe('')
+    })
+})
+
+describe('Non-number or non-string', () => {
+    function invalidInput(input) {
+        return () => prettyCurrency(input)
+    }
+
+    const error = new TypeError('parameter must be a number or string.')
+
+    test('boolean should throw a TypeError', () => {
+        expect(invalidInput(false)).toThrowError(error)
+    })
+
+    test('null should throw a TypeError', () => {
+        expect(invalidInput(null)).toThrowError(error)
+    })
+
+    test('object should throw a TypeError', () => {
+        expect(invalidInput({})).toThrowError(error)
+    })
+
+    test('undefined should throw a TypeError', () => {
+        expect(invalidInput(undefined)).toThrowError(error)
+    })
+
+    test('function should throw a TypeError', () => {
+        expect(invalidInput(function() {})).toThrowError(error)
+    })
+
+    test('symbol should throw a TypeError', () => {
+        expect(invalidInput(Symbol(''))).toThrowError(error)
+    })
 })
