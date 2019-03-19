526f74cb98a28964783e4566fab51aef19f05580
venus <venus@venusworld.cn>
2019-03-19 16:46:00 +0800

    fix NaN parameter bug.

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ function prettyCurrency(input) {
         throw new TypeError('parameter must be a number or string.')
     }
 
+    const parsedInput = Number.parseFloat(input)
+    if (Number.isNaN(parsedInput) && input !== '') {
+        throw new Error('parameter is invalid.')
+    }
+
+    input = Number.isNaN(parsedInput) ? input : parsedInput
     let [ integer, decimal ] = input.toString().split('.')
     integer = integer.replace(/\B(?=(\d{3})+$)/g, ',')
 
