bb2bc719b53064135cd43ac5879de7bd79e3d934
venus <venus@venusworld.cn>
2019-03-19 16:46:09 +0800

    add test cases.

--- test/index.test.js
+++ test/index.test.js
@@ -1,5 +1,9 @@
 const prettyCurrency = require('pretty-currency')
 
+function invalidInput(input) {
+    return () => prettyCurrency(input)
+}
+
 describe('Integer', () => {
     test('123456 should be "123,456"', () => {
         expect(prettyCurrency(123456)).toBe('123,456')
@@ -24,6 +28,10 @@ describe('Integer', () => {
     test('-1234 should be "-1,234"', () => {
         expect(prettyCurrency(-1234)).toBe('-1,234')
     })
+
+    test('-0.1234 should be "-0.1234"', () => {
+        expect(prettyCurrency(-0.1234)).toBe('-0.1234')
+    })
 })
 
 describe('Float number', () => {
@@ -44,7 +52,9 @@ describe('Float number', () => {
     })
 
     test('123456.00 should be "123,456"', () => {
-        // JS engine will transform float number 123.00 to 123 integer
+        // JS engine will transform float number 123.00 to 123 integer,
+        // according to IEEE-754, number will be stored 64-bit binary,
+        // both float number and integer.
         expect(prettyCurrency(123456.00)).toBe('123,456')
     })
 
@@ -73,13 +83,23 @@ describe('String', () => {
     test('"" should be ""', () => {
         expect(prettyCurrency('')).toBe('')
     })
+
+    test('started with non-number should throw an Error.', () => {
+        expect(invalidInput('invalid string')).toThrowError(/parameter is invalid/)
+        expect(invalidInput('ss123')).toThrowError(/parameter is invalid/)
+        expect(invalidInput('ss1234.1234')).toThrowError(/parameter is invalid/)
+        expect(invalidInput('ss1234.1234ss')).toThrowError(/parameter is invalid/)
+    })
+
+    test('started with number should be considered as a valid parameter.', () => {
+        expect(prettyCurrency('123sss')).toBe('123')
+        expect(prettyCurrency('12345sss')).toBe('12,345')
+        expect(prettyCurrency('12345sss1234')).toBe('12,345')
+        expect(prettyCurrency('12345sss1234sss')).toBe('12,345')
+    })
 })
 
 describe('Non-number or non-string', () => {
-    function invalidInput(input) {
-        return () => prettyCurrency(input)
-    }
-
     const error = new TypeError('parameter must be a number or string.')
 
     test('boolean should throw a TypeError', () => {
